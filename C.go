44cc994e1d11c52e9e410db59e453bbad5f3a6ce
beck <yangqingbo@safeis.cn>
2022-06-29 10:56:23 +0800

    remove

--- C.go
+++ C.go
@@ -9,15 +9,4 @@ type Chi struct {
 func (chi *Chi) Chi_echo() {
 	fmt.Println("chile ...")
 	fmt.Println(chi.Echo())
-	fmt.Println("c1")
-	fmt.Println("c100")
-	fmt.Println("c200")
-	fmt.Println("c300")
-	fmt.Println("c400")
-	fmt.Println("c500")
-	fmt.Println("c600")
-	fmt.Println("c700")
-	fmt.Println("m1")
-	fmt.Println("m2")
-	fmt.Println("m3")
 }
